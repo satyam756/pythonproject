496c17f26b45a521b133d21fc1937de89d67fb8e
satyam756 <57908190+satyam756@users.noreply.github.com>
2020-03-22 19:45:13 +0530

    Update TIc.py

--- TIc.py
+++ TIc.py
@@ -1,7 +1,7 @@
 board = {
-    'T1': ' ', 'T2': ' ', 'T3': ' ',
-    'M1': ' ', 'M2': ' ', 'M3': ' ',
-    'D1': ' ', 'D2': ' ', 'D3': ' '
+    '0': ' ', '1': ' ', '2': ' ',
+    '3': ' ', '4': ' ', '5': ' ',
+    '6': ' ', '7': ' ', '8': ' '
 }
 
 player = 1          # to initialise first player
@@ -12,71 +12,71 @@ end_check = 0
 def check():
     # checking the moves of player one
     # for horizontal(start)
-    if board['T1'] == 'X' and board['T2'] == 'X' and board['T3'] == 'X':
+    if board['0'] == 'X' and board['1'] == 'X' and board['2'] == 'X':
         print('Player one won !')
         return 1
-    if board['M1'] == 'X' and board['M2'] == 'X' and board['M3'] == 'X':
+    if board['3'] == 'X' and board['4'] == 'X' and board['5'] == 'X':
         print('Player One Won!!')
         return 1
-    if board['D1'] == 'X' and board['D2'] == 'X' and board['D3'] == 'X':
+    if board['6'] == 'X' and board['7'] == 'X' and board['8'] == 'X':
         print('Player One Won!!')
         return 1
     # for horizontal(end)
     # for diagonal(start)
-    if board['T1'] == 'X' and board['M2'] == 'X' and board['D3'] == 'X':
+    if board['1'] == 'X' and board['4'] == 'X' and board['8'] == 'X':
         print('Player One Won!!')
         return 1
     # for diagonal(end)
     # for vertical(start)
-    if board['T1'] == 'X' and board['M1'] == 'X' and board['D1'] == 'X':
+    if board['0'] == 'X' and board['3'] == 'X' and board['6'] == 'X':
         print('Player One Won!!')
         return 1
-    if board['T2'] == 'X' and board['M2'] == 'X' and board['D2'] == 'X':
+    if board['1'] == 'X' and board['4'] == 'X' and board['7'] == 'X':
         print('Player One Won!!')
         return 1
-    if board['T3'] == 'X' and board['M3'] == 'X' and board['D3'] == 'X':
+    if board['2'] == 'X' and board['5'] == 'X' and board['8'] == 'X':
         print('Player One Won!!')
         return 1
     # for vertical(end)
 
     # checking the moves of player two
-    if board['T1'] == 'O' and board['T2'] == 'O' and board['T3'] == 'O':
+    if board['0'] == 'O' and board['1'] == 'O' and board['2'] == 'O':
         print('Player Two Won!!')
         return 1  # used to end the game
-    if board['M1'] == 'O' and board['M2'] == 'O' and board['M3'] == 'O':
+    if board['3'] == 'O' and board['4'] == 'O' and board['5'] == 'O':
         print('Player Two Won!!')
         return 1
-    if board['D1'] == 'O' and board['D2'] == 'O' and board['D3'] == 'O':
+    if board['6'] == 'O' and board['7'] == 'O' and board['8'] == 'O':
         print('Player Two Won!!')
         return 1
-    if board['T1'] == 'O' and board['M2'] == 'O' and board['D3'] == 'O':
+    if board['0'] == 'O' and board['4'] == 'O' and board['8'] == 'O':
         print('Player Two Won!!')
         return 1
-    if board['T1'] == 'O' and board['M1'] == 'O' and board['D1'] == 'O':
+    if board['0'] == 'O' and board['3'] == 'O' and board['6'] == 'O':
         print('Player Two Won!!')
         return 1
-    if board['T2'] == 'O' and board['M2'] == 'O' and board['D2'] == 'O':
+    if board['1'] == 'O' and board['4'] == 'O' and board['7'] == 'O':
         print('Player Two Won!!')
         return 1
-    if board['T3'] == 'O' and board['M3'] == 'O' and board['D3'] == 'O':
+    if board['2'] == 'O' and board['5'] == 'O' and board['8'] == 'O':
         print('Player Two Won!!')
         return 1
     return 0
 
 
-print('T1|T2|T3')
+print('0|1|2')
 print('- +- +-')
-print('M1|M2|M3')
+print('3|4|5')
 print('- +- +-')
-print('D1|D2|D3')
+print('6|7|8')
 print('***************************')
 
 while True:
-    print(board['T1']+'|'+board['T2']+'|'+board['T3'])
+    print(board['0']+'|'+board['1']+'|'+board['2'])
     print('-+-+-')
-    print(board['M1'] + '|' + board['M2'] + '|' + board['M3'])
+    print(board['3'] + '|' + board['4'] + '|' + board['5'])
     print('-+-+-')
-    print(board['D1'] + '|' + board['D2'] + '|' + board['D3'])
+    print(board['6'] + '|' + board['7'] + '|' + board['8'])
     end_check = check()
     if total_moves == 9 or end_check == 1:
         break
@@ -106,4 +106,5 @@ while True:
 
 
 
-#Tic_Tac_Toe.txt
+#Tic_Tac_Toe
+
